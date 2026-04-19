430a30d4235f2313df2e8f83e3f2199918bdfb8f
AtharvAlkunte <alkunteatharva19@gmail.com>
2026-04-19 21:58:48 +0530

    fixed router issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
+
 import Home from './client/pages/Home';
 import ServicesPage from './client/pages/Services';
 import MyBookings from './client/pages/MyBookings';
 import Safety from './client/pages/Safety';
-
 import Admin from './server/pages/Admin';
+
 import { BookingProvider } from './context/BookingContext';
 import Navbar from './client/components/Navbar';
 import Footer from './client/components/Footer';
@@ -24,20 +25,18 @@ const ClientLayout = () => (
 function App() {
   return (
     <BookingProvider>
-      <Router>
-        <Routes>
-          {/* Client Routes with Navbar and Footer */}
-          <Route path="/" element={<ClientLayout />}>
-            <Route index element={<Home />} />
-            <Route path="services" element={<ServicesPage />} />
-            <Route path="my-bookings" element={<MyBookings />} />
-            <Route path="safety" element={<Safety />} />
-          </Route>
+      <Routes>
+        {/* Client Routes */}
+        <Route path="/" element={<ClientLayout />}>
+          <Route index element={<Home />} />
+          <Route path="services" element={<ServicesPage />} />
+          <Route path="my-bookings" element={<MyBookings />} />
+          <Route path="safety" element={<Safety />} />
+        </Route>
 
-          {/* Admin Routes without Navbar and Footer */}
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </Router>
+        {/* Admin Route */}
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
     </BookingProvider>
   );
 }
